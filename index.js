1347d862371e9a9ecaf5b2b6024c5abee7a3cf55
Александр Русак <a.rusak@corp.lenvendo.ru>
2017-09-01 21:42:58 +0300

    module, public methods

--- index.js
+++ index.js
@@ -1,127 +1,172 @@
-"use strict";
+var MyForm = (function() {
+  "use strict";
 
-const form = document.getElementById("myForm");
-const email = form.elements.email;
+  class YandexForm {
+    constructor(form) {
+      this.form = form;
+      this.isValid = false;
+      this.fields = ["fio", "email", "phone"];
+      this.errorFields = new Set(this.fields);
+      this.inProgress = false;
 
-class YandexForm {
-  constructor(form) {
-    this.form = form;
-    this.inputs = {
-      fio: this.form.elements.fio,
-      email: this.form.elements.email,
-      phone: this.form.elements.phone
-    };
-    this.addHandlers();
-  }
+      this.domButton = this.form.elements.submitButton;
+      this.domResultContainer = document.getElementById("resultContainer");
+    }
 
-  validators(value) {
-    return {
-      isPhoneValid() {
-        const regex = /^\+7\(\d\d\d\)\d\d\d-\d\d-\d\d$/;
-        const PHONE_MAX_SUM = 30;
-        const digits = value.match(/\d/g);
-        let sum = 0;
-        if (digits) {
-          sum = digits.reduce((acc, el) => acc + +el, sum);
+    get validators() {
+      return {
+        phone(value) {
+          const regex = /^\+7\(\d\d\d\)\d\d\d-\d\d-\d\d$/;
+          const PHONE_MAX_SUM = 30;
+          const digits = value.match(/\d/g);
+          let sum = 0;
+          if (digits) {
+            sum = digits.reduce((acc, el) => acc + +el, sum);
+          }
+
+          return regex.test(value) && digits && sum <= PHONE_MAX_SUM;
+        },
+        email(value) {
+          // RFC 5322 Official Standard
+          const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          const DOMAINS = [
+            "ya.ru",
+            "yandex.ru",
+            "yandex.ua",
+            "yandex.by",
+            "yandex.kz",
+            "yandex.com"
+          ];
+
+          return regex.test(value) && DOMAINS.includes(value.split("@")[1]);
+        },
+        fio(value) {
+          return value.trim().split(/\s+/).length === 3;
         }
+      };
+    }
 
-        return regex.test(value) && digits && sum <= PHONE_MAX_SUM;
+    checkValidity() {
+      this.isValid = true;
+
+      this.fields.forEach(name => {
+        const field = this.form.elements[name];
+        const isValid = this.validators[name](field.value);
+
+        if (isValid) {
+          this.errorFields.delete(name);
+        } else {
+          this.errorFields.add(name);
+          this.isValid = false;
+        }
+        field.isValid = isValid;
+      });
+
+      return this.isValid;
+    }
+
+    setValidity() {
+      this.fields.forEach(name => {
+        const field = this.form.elements[name];
+        if (field.isValid) {
+          field.classList.remove("error");
+        } else {
+          field.classList.add("error");
+        }
+      });
+    }
+
+    setFields(data) {
+      this.fields.forEach(name => {
+        if (data[name]) {
+          const field = this.form.elements[name];
+          field.value = data[name];
+        }
+      });
+    }
+
+    setContainer(result) {
+      switch (result.status) {
+        case "success":
+          this.domResultContainer.textContent = "Success";
+          break;
+        case "error":
+          this.domResultContainer.textContent = result.reason;
+          break;
+        case "progress":
+          setTimeout(() => {
+            this.response();
+          }, result.timeout);
+          break;
+        default:
+          throw new Error("Not valid response from server: " + result);
       }
+      this.domResultContainer.className = result.status;
+    }
+
+    get response() {
+      return async () => {
+        let response = await fetch(form.action);
+        let status = await response.json();
+        this.setContainer(status);
+
+        //this.domButton.removeAttribute("disabled");
+      };
+    }
+
+    onSubmit(event) {
+      event && event.preventDefault();
+
+      const isFormValid = this.checkValidity();
+      if (isFormValid) {
+        this.response();
+        this.domButton.setAttribute("disabled", "disabled");
+      }
+      this.setValidity();
+    }
+  }
+
+  ///
+
+  const form = document.getElementById("myForm");
+  const yandexForm = new YandexForm(form);
+  form.addEventListener("submit", yandexForm.onSubmit.bind(yandexForm));
+
+  ///
+
+  const validate = () => {
+    var errorFields = function() {
+      yandexForm.checkValidity();
+      return [...yandexForm.errorFields];
     };
-  }
 
-  addHandlers() {
-    const phone = this.inputs.phone;
-    phone.addEventListener("input", () => {
-      console.log(phone.name, this.validators(phone.value).isPhoneValid());
+    return {
+      isValid: yandexForm.checkValidity(),
+      errorFields: errorFields()
+    };
+  };
+
+  const getData = () => {
+    var obj = {};
+    yandexForm.fields.forEach(function(name) {
+      var value = yandexForm.form.elements[name].value;
+      obj[name] = value;
     });
-  }
-}
+    return obj;
+  };
 
-new YandexForm(document.getElementById("myForm"));
+  const setData = data => {
+    return yandexForm.setFields(data);
+  };
 
-const isPhoneValid = value => {
-  const regex = /^\+7\(\d\d\d\)\d\d\d-\d\d-\d\d$/;
-  const PHONE_MAX_SUM = 30;
-  const digits = value.match(/\d/g);
-  let sum = 0;
-  if (digits) {
-    sum = digits.reduce((acc, el) => acc + +el, sum);
-  }
+  const submit = () => {
+    yandexForm.onSubmit();
+  };
 
-  return regex.test(value) && digits && sum <= PHONE_MAX_SUM;
-};
-// +7(971)111-11-11
-
-const isFioValid = value => {
-  return value.trim().split(/\s+/).length === 3;
-};
-
-const isEmailValid = value => {
-  // RFC 5322 Official Standard
-  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  const DOMAINS = [
-    "ya.ru",
-    "yandex.ru",
-    "yandex.ua",
-    "yandex.by",
-    "yandex.kz",
-    "yandex.com"
-  ];
-
-  return regex.test(value) && DOMAINS.includes(value.split("@")[1]);
-};
-
-function max_sum_of_numbers(field, maxSum) {
-  var nums = field.match(/\d/g),
-    sum = 0;
-
-  for (var i = 0; i < nums.length; i++) {
-    sum += parseInt(nums[i]);
-  }
-  console.log(sum);
-
-  return sum <= maxSum;
-}
-
-form.elements.fio.addEventListener("input", function() {
-  console.log(this.name, isFioValid(this.value));
-});
-form.elements.email.addEventListener("input", function() {
-  console.log(this.name, isEmailValid(this.value));
-});
-form.elements.phone.addEventListener("input", function() {
-  //console.log(this.name, isPhoneValid(this.value));
-});
-
-[...form.elements].forEach(element => {
-  if (element.tagName === "INPUT") {
-    //eventHandler(element);
-  }
-});
-
-function eventHandler(element) {
-  element.addEventListener("input", () => {
-    let specialCaseForValidation = true;
-
-    if (element.name === "phone") {
-      specialCaseForValidation = max_sum_of_numbers(element.value, 24);
-    }
-
-    var isValid =
-      !element.validity.valueMissing &&
-      regex[element.name].test(element.value) &&
-      specialCaseForValidation;
-
-    if (isValid) {
-      element.classList.remove("error");
-    } else {
-      element.classList.add("error");
-    }
-    console.log(element.name, isValid);
-  });
-}
-
-form.addEventListener("submit", event => {
-  event.preventDefault();
-});
+  return {
+    validate,
+    getData,
+    setData,
+    submit
+  };
+})();
---
+++ server/server.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.', {
+  cache: 0
+});
+
+function accept(request, response) {
+
+  if (request.url.slice(0, 6) === '/data/') {
+    setTimeout(() => {
+      response.setHeader("Access-Control-Allow-Origin", '*');
+
+      file.serve(request, response);
+    }, 500);
+  } else {
+    //request.url = '/public' + request.url;
+    file.serve(request, response);
+  }
+}
+
+http.createServer(accept).listen(3000);
+console.log('Server running on port 3000');
